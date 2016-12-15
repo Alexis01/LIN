27319369afbe59456cf52910d92a9c3e097a87ea
Alexis Cumbal <genericmail0001@gmail.com>
2016-12-15 13:37:36 +0100

    pr3 b implemented

--- pr3/b/fifoproc.c
+++ pr3/b/fifoproc.c
@@ -1,5 +1,5 @@
 /*
- *  Module that implements a list
+ *  Module that implements a fifo 
  */
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -13,11 +13,11 @@
 #include "cbuffer.c"
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("List module - FDI-UCM");
+MODULE_DESCRIPTION("fifoproc Kernel Module - FDI-UCM");
 MODULE_AUTHOR("Alexis Cumbal Calderón");
 
 #define BUFFER_LENGTH       PAGE_SIZE/8
-
+#define MAX_CBUFFER_LEN 100
 /******Resources*********/
 //buffer
 cbuffer_t* cbuffer; /* Buffer circular */
@@ -31,13 +31,141 @@ truct semaphore sem_cons; /* cola de espera para consumidor(es) */
 //Dos variables condicionales
 int nr_prod_waiting=0; /* Número de procesos productores esperando */
 int nr_cons_waiting=0; /* Número de procesos consumidores esperando */
-
-
-
+//Entrada /proc
 static struct proc_dir_entry *proc_entry;
-static char *modlist;  // Space for the "modlist"
 
+static int fifoproc_open(struct inode *inode, struct file *file){
+	if( down_interruptible( &mtx ) ){
+		return -EINTR;
+	}
+	if( file->f_mode & FMODE_READ ){ //consumidor
+		cons_count += 1;
+		up( &sem_prod );
+		nr_prod_waiting -= 1;
+		while( prod_count == 0 ){
+			nr_cons_waiting += 1;
+			up( &mtx );
+			if( down_interruptible( &sem_cons ) ){
+				return -EINTR;
+			}
+		}
+	}else{//productor
+		prod_count += 1;
+		up( &sem_cons );
+		nr_cons_waiting -= 1;
+		while( cons_count == 0 ){
+			nr_prod_waiting += 1;
+			up( &mtx );
+			if( down_interruptible( &sem_prod ) ){
+				return -EINTR;
+			}
+		}
+	}
+	up( &mtx );
+	return 0;
+}
+static int fifoproc_release(struct inode *inode, struct file *file){
+	if( down_interruptible( &mtx ) ){
+		return -EINTR;
+	}
+	if( file->f_mode & FMODE_READ ){ //consumidor
+		cons_count -= 1;
+		if( nr_prod_waiting > 0 ){
+			up( &sem_cons );
+			nr_cons_waiting -=1;
+		}
+	}else{//productor
+		prod_count -=1;
+		if( nr_cons_waiting > 0 ){
+			up( &sem_cons );
+			nr_cons_waiting -= 1;
+		}
+	}
+	if( prod_count == 0 && cons_count == 0 ){
+		clear_cbuffer_t( cbuffer );
+	}
+	up( &mtx );
+	return 0;
+}
+static ssize_t fifoproc_read(struct file *file, char __user *buf, size_t len, loff_t *off){
+	char kbuffer[MAX_CBUFFER_LEN];
+	if( len > MAX_CBUFFER_LEN ){
+		return -EFAULT;
+	}
+	
+	if( down_interruptible( &mtx ) ){
+		return -EINTR;
+	}
+	
+	while( size_cbuffer_t( cbuffer ) < len && prod_count > 0 ){
+		nr_cons_waiting += 1;
+		up( &mtx );
+		if( down_interruptible( &sem_cons ) ){
+			return -EINTR;
+		}
+	}
+	
+	if( prod_count == 0 && size_cbuffer_t( cbuffer ) == 0 ){
+		up( &mtx );
+		return 0;
+	}
+	
+	remove_items_cbuffer_t( cbuffer, kbuffer, len );
 
+	if( nr_prod_waiting > 0 ){
+		up( &sem_prod );
+		nr_prod_waiting -= 1;
+	}
+
+	up( &mtx );
+
+	if( copy_to_user( buf, kbuffer, len ) ){
+		return -EFAULT;
+	}
+	return len;
+}
+static ssize_t fifoproc_write(struct file *file, const char __user *buf, size_t len, loff_t *off){
+	char kbuffer[MAX_CBUFFER_LEN];
+
+	if( len > MAX_CBUFFER_LEN ){
+		return -EFAULT;
+	}	
+
+	if( copy_from_user( kbuffer, buf, len ) ){
+		return -EFAULT;
+	}
+
+	if( down_interruptible( &mtx ) ){
+		return -EINTR;
+	}
+
+	/* Esperar hasta que haya hueco para insertar (debe haber consumidores) */
+	while( nr_gaps_cbuffer_t( cbuffer ) < len && cons_count > 0 ){
+		nr_prod_waiting += 1;
+		up( &mtx );
+		if( down_interruptible( &sem_prod ) ){
+			return -EINTR;
+		}
+	}
+
+	/* Detectar fin de comunicación por error (consumidor cierra FIFO antes) */
+	if( cons_count == 0 ){
+		up( &mtx );
+		return -EPIPE;
+	}
+
+	insert_items_cbuffer_t( cbuffer, kbuffer, len );
+
+	/* Despertar a posible consumidor bloqueado */
+	if( nr_cons_waiting > 0 ){
+		up( &sem_cons );
+		nr_cons_waiting -= 1;
+	}
+
+	up( &mtx );
+	return len;
+
+}
 
 static const struct file_operations proc_entry_fops = {
 	.read = fifoproc_read,
@@ -45,6 +173,33 @@ static const struct file_operations proc_entry_fops = {
     .open = fifoproc_open,
     .release = fifoproc_release
 };
+
+static int __init init_module(void){
+	
+    if( createEntry() != 0 ){
+        printk(KERN_INFO "Cannot create entry!!\n");
+    	return -ENOSPC;
+    }else{
+    	//Inicializamos semaforos de mutex y de colas de espera
+		sema_init( &mtx, 1 );
+		sema_init( &sem_prod, 0 );
+		sema_init( &sem_cons, 0 );
+		//Inicializamos el buffer 
+		cbuffer = create_cbuffer_t(MAX_CBUFFER_LEN);
+		printk(KERN_INFO "fifoproc: Loaded\n");
+	    return 0;
+    }
+	
+}
+
+static void __exit cleanup_module(void){
+
+	remove_proc_entry("fifoproc", NULL);
+	vfree( modlist );
+	printk(KERN_INFO "fifoproc: Removed\n");
+}
+
+
 
 int createEntry(void){
     int ret = 0;
@@ -54,106 +209,20 @@ int createEntry(void){
 		ret = -ENOMEM;
 	}else{
 		memset( modlist, 0, BUFFER_LENGTH );
-		proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops );
+		proc_entry = proc_create( "fifoproc", 0666, NULL, &proc_entry_fops );
 		if( proc_entry == NULL ){
 			ret = -ENOMEM;
 			vfree( modlist );
-			printk(KERN_INFO "Modlist: Can't create /proc/modlist entry\n");
+			printk(KERN_INFO "fifoproc: Can't create /proc/fifoproc entry\n");
 		}else{
-			printk(KERN_INFO "Modlist: Creates /proc/modlist entry\n");
+			printk(KERN_INFO "fifoproc: Creates /proc/fifoproc entry\n");
 		} 
 	}
 	return ret;
 }
-/* Se invoca al hacer open() de entrada /proc */
-static int fifoproc_open(struct inode *, struct file *){
-	if(file->f_mode & FMODE_READ){
-		/* Un consumidor abrió el FIFO */
-	
-	}else{
-		/* Un productor abrió el FIFO */
-	
-	}
-}
-/* Se invoca al hacer close() de entrada /proc */
-static int fifoproc_release(struct inode *, struct file *){
 
-}
-/* Se invoca al hacer read() de entrada /proc */
-static ssize_t fifoproc_read(struct file *, char *, size_t, loff_t *){
-
-}
-/* Se invoca al hacer write() de entrada /proc */
-static ssize_t fifoproc_write(struct file *, const char *, size_t, wloff_t *){
-
-}
-void fifoproc_open(bool abre_para_lectura) {
-	/* Completar */
-
-}
-int fifoproc_write(char* buff, int len) {
-	/* Completar */
-	char kbuffer[MAX_KBUF];
-
-	if (len> MAX_CBUFFER_LEN || len> MAX_KBUF) { 
-		return Error;
-	}
-
-	if (copy_from_user(kbuffer,buff,len)) { 
-		return Error;
-	}
-
-	lock(mtx);
-	/* Esperar hasta que haya hueco para insertar (debe haber consumidores) */
-	while (nr_gaps_cbuffer_t(cbuffer)<len && cons_count>0){
-		cond_wait(prod,mtx);
-	}
-	/* Detectar fin de comunicación por error (consumidor cierra FIFO antes) */
-	if (cons_count==0) {
-		unlock(mtx); 
-		return -EPIPE;
-	}
-	insert_items_cbuffer_t(cbuffer,kbuffer,len);
-	/* Despertar a posible consumidor bloqueado */
-	cond_signal(cons);
-	
-	unlock(mtx);
-	
-	return len;
-}
-int fifoproc_read(const char* buff, int len) {
-	/* Completar */
-
-}
-void fifoproc_release(bool lectura) {
-	/* Completar */
-
-}
-static int __init init_module(void){
-	
-    if( createEntry() != 0 ){
-        printk(KERN_INFO "Cannot create entry!!\n");
-    	return -ENOSPC;
-    }else{
-    	INIT_LIST_HEAD(&my_list);// ‘struct list_item’ has no member named ‘list’
-		printk(KERN_INFO "ListMod: Loaded\n");
-	    return 0;
-    }
-	
-}
-
-
-
-
-static void __exit cleanup_module(void){
-
-	remove_proc_entry("modlist", NULL);
-	vfree( modlist );
-	printk(KERN_INFO "ListMod: Removed\n");
-}
-
-module_init(list_module_init);
-module_exit(list_module_exit);
+module_init(init_module);
+module_exit(cleanup_module);
 
 
 
